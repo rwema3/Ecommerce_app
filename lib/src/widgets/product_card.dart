30e369f5a88b907cff2aafc57b5a9cb470eae0a1
Dominique Rwema Bagirishya <52289151+rwema3@users.noreply.github.com>
2022-10-03 16:58:56 -0700

    Update product_card.dart

--- lib/src/widgets/product_card.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-
-import 'package:flutter_ecommerce_app/src/model/product.dart';
-import 'package:flutter_ecommerce_app/src/themes/light_color.dart';
-import 'package:flutter_ecommerce_app/src/widgets/title_text.dart';
-import 'package:flutter_ecommerce_app/src/widgets/extentions.dart';
