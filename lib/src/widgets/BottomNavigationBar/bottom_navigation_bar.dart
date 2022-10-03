26d873923350773e9ab64c9d857eda02de9cddd7
Dominique Rwema Bagirishya <52289151+rwema3@users.noreply.github.com>
2022-10-03 16:59:49 -0700

    Update bottom_navigation_bar.dart

--- lib/src/widgets/BottomNavigationBar/bottom_navigation_bar.dart
+++ lib/src/widgets/BottomNavigationBar/bottom_navigation_bar.dart
@@ -1,13 +1,3 @@
   @override
   void dispose() {
     _xController.dispose();
-                _icon(Icons.card_travel, _selectedIndex == 2, 2),
-                _icon(Icons.favorite_border, _selectedIndex == 3, 3),
-              ],
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-}
