34794f284447a569dfb6f4358bbdf481eb0780b9
Dominique Rwema Bagirishya <52289151+rwema3@users.noreply.github.com>
2022-10-03 16:59:46 -0700

    Update bottom_navigation_bar.dart

--- lib/src/widgets/BottomNavigationBar/bottom_navigation_bar.dart
+++ lib/src/widgets/BottomNavigationBar/bottom_navigation_bar.dart
@@ -1,16 +1,6 @@
   @override
   void dispose() {
     _xController.dispose();
-          Positioned(
-            left: (appSize.width - _getButtonContainerWidth()) / 2,
-            top: 0,
-            width: _getButtonContainerWidth(),
-            height: height,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: <Widget>[
-                _icon(Icons.home, _selectedIndex == 0, 0),
-                _icon(Icons.search, _selectedIndex == 1, 1),
                 _icon(Icons.card_travel, _selectedIndex == 2, 2),
                 _icon(Icons.favorite_border, _selectedIndex == 3, 3),
               ],
