879e312a3bc3167e5bb4b48208e7741d07acbc46
Dominique Rwema Bagirishya <52289151+rwema3@users.noreply.github.com>
2022-10-03 17:03:04 -0700

    Update product_card.dart

--- lib/src/widgets/product_card.dart
+++ lib/src/widgets/product_card.dart
@@ -4,12 +4,6 @@ import 'package:flutter_ecommerce_app/src/model/product.dart';
 import 'package:flutter_ecommerce_app/src/themes/light_color.dart';
 import 'package:flutter_ecommerce_app/src/widgets/title_text.dart';
 import 'package:flutter_ecommerce_app/src/widgets/extentions.dart';
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        color: LightColor.background,
         borderRadius: BorderRadius.all(Radius.circular(20)),
         boxShadow: <BoxShadow>[
           BoxShadow(color: Color(0xfff8f8f8), blurRadius: 15, spreadRadius: 10),
