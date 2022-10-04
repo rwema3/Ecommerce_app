a362cd8f4a51dfccf7d3843fcad4a6b6da5319bf
Dominique Rwema Bagirishya <52289151+rwema3@users.noreply.github.com>
2022-10-03 17:05:28 -0700

    Update product_card.dart

--- lib/src/widgets/product_card.dart
+++ lib/src/widgets/product_card.dart
@@ -4,7 +4,11 @@ import 'package:flutter_ecommerce_app/src/model/product.dart';
 import 'package:flutter_ecommerce_app/src/themes/light_color.dart';
 import 'package:flutter_ecommerce_app/src/widgets/title_text.dart';
 import 'package:flutter_ecommerce_app/src/widgets/extentions.dart';
-per(key: key);
+
+class ProductCard extends StatelessWidget {
+  final Product product;
+  final ValueChanged<Product> onSelected;
+  ProductCard({Key key, this.product, this.onSelected}) : super(key: key);
 
 //   @override
 //   _ProductCardState createState() => _ProductCardState();
