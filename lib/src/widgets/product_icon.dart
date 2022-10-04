2e1fadb84c8bcbd953f048803800ffac5c4fe911
Dominique Rwema Bagirishya <52289151+rwema3@users.noreply.github.com>
2022-10-03 17:05:35 -0700

    Update product_icon.dart

--- lib/src/widgets/product_icon.dart
+++ lib/src/widgets/product_icon.dart
@@ -1,3 +1,9 @@
+
+  Widget build(BuildContext context) {
+    return model.id == null
+        ? Container(width: 5)
+        : Container(
+            margin: EdgeInsets.symmetric(horizontal: 10, vertical: 15),
             child: Container(
               padding: AppTheme.hPadding,
               alignment: Alignment.center,
