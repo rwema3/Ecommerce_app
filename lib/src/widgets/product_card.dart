a313405761e0af7a884e162a2934ffdc6d729b45
Dominique Rwema Bagirishya <52289151+rwema3@users.noreply.github.com>
2022-10-03 17:03:33 -0700

    Update product_card.dart

--- lib/src/widgets/product_card.dart
+++ lib/src/widgets/product_card.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/material.dart';
 
 import 'package:flutter_ecommerce_app/src/model/product.dart';
-import 'package:flutter_ecommerce_app/src/themes/light_color.dart';
-import 'package:flutter_ecommerce_app/src/widgets/title_text.dart';
-import 'package:flutter_ecommerce_app/src/widgets/extentions.dart';
       ),
       margin: EdgeInsets.symmetric(vertical: !product.isSelected ? 20 : 0),
       child: Container(
