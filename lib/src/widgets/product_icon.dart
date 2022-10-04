f713920e1b67992ad3daa22c44c78493efd2abfc
Dominique Rwema Bagirishya <52289151+rwema3@users.noreply.github.com>
2022-10-03 17:05:40 -0700

    Update product_icon.dart

--- lib/src/widgets/product_icon.dart
+++ lib/src/widgets/product_icon.dart
@@ -1,3 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_ecommerce_app/src/model/category.dart';
+import 'package:flutter_ecommerce_app/src/themes/light_color.dart';
+import 'package:flutter_ecommerce_app/src/themes/theme.dart';
+import 'package:flutter_ecommerce_app/src/widgets/title_text.dart';
+import 'package:flutter_ecommerce_app/src/widgets/extentions.dart';
+
 class ProductIcon extends StatelessWidget {
   // final String imagePath;
   // final String text;
