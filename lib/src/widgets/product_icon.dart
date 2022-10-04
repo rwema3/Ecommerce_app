b7bfe3bf0b4fd02ada406644dae962b8b41580e0
Dominique Rwema Bagirishya <52289151+rwema3@users.noreply.github.com>
2022-10-03 17:01:39 -0700

    Update product_icon.dart

--- lib/src/widgets/product_icon.dart
+++ lib/src/widgets/product_icon.dart
@@ -1,19 +1,5 @@
         ? Container(width: 5)
         : Container(
-              child: Row(
-                children: <Widget>[
-                  model.image != null ? Image.asset(model.image) : SizedBox(),
-                  model.name == null
-                      ? Container()
-                      : Container(
-                          child: TitleText(
-                            text: model.name,
-                            fontWeight: FontWeight.w700,
-                            fontSize: 15,
-                          ),
-                        )
-                ],
-              ),
             ).ripple(
               () {
                 onSelected(model);
