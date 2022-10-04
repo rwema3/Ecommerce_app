37c66d5712c21aca50b3106a154175ce7c7934ea
Dominique Rwema Bagirishya <52289151+rwema3@users.noreply.github.com>
2022-10-03 17:02:23 -0700

    Update product_icon.dart

--- lib/src/widgets/product_icon.dart
+++ lib/src/widgets/product_icon.dart
@@ -1,5 +1,15 @@
         ? Container(width: 5)
         : Container(
+            
+                boxShadow: <BoxShadow>[
+                  BoxShadow(
+                    color: model.isSelected ? Color(0xfffbf2ef) : Colors.white,
+                    blurRadius: 10,
+                    spreadRadius: 5,
+                    offset: Offset(5, 5),
+                  ),
+                ],
+              ),
               child: Row(
                 children: <Widget>[
                   model.image != null ? Image.asset(model.image) : SizedBox(),
