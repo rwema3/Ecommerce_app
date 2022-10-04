092b81c5e1514663a1af50059c93f690ace5ee56
Dominique Rwema Bagirishya <52289151+rwema3@users.noreply.github.com>
2022-10-03 17:04:45 -0700

    Update centered_elasticIn_curve.dart

--- lib/src/widgets/BottomNavigationBar/centered_elasticIn_curve.dart
+++ lib/src/widgets/BottomNavigationBar/centered_elasticIn_curve.dart
@@ -1,4 +1,15 @@
 import 'dart:math' as math;
+
+  CenteredElasticOutCurve([this.period = 0.4]);
+
+  @override
+  double transform(double x) {
+    // Bascially just a slightly modified version of the built in ElasticOutCurve
+    return math.pow(2.0, -10.0 * x) * math.sin(x * 2.0 * math.pi / period) +
+        0.5;
+  }
+}
+
 class CenteredElasticInCurve extends Curve {
   final double period;
 
