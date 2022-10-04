a7d08c79881c2a84669674fd7a1696d65d384f12
Dominique Rwema Bagirishya <52289151+rwema3@users.noreply.github.com>
2022-10-03 17:04:50 -0700

    Update product_card.dart

--- lib/src/widgets/product_card.dart
+++ lib/src/widgets/product_card.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 
 import 'package:flutter_ecommerce_app/src/model/product.dart';
+      ),
+      margin: EdgeInsets.symmetric(vertical: !product.isSelected ? 20 : 0),
+      child: Container(
+        padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
         child: Stack(
           alignment: Alignment.center,
           children: <Widget>[
